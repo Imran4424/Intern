d0f3cc95bf302cfa97cc31dbb98e3f6e2c06cb5f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-29 00:24:17 +0600

    set 1

--- Set 1/10664.Luggage.cpp
+++ Set 1/10664.Luggage.cpp
@@ -1,9 +1,58 @@
 #include <iostream>
 using namespace std;
 
+const int mSize = 21;
+const int wSize = 201;
+
+int luggages[mSize];
+int dpWeight[mSize][wSize];
+
+int totalSum, totalObject;
+
+void Init()
+{
+	for (int i = 0; i <= totalObject; ++i)
+	{
+		for (int i = 0; i <= totalSum; ++i)
+		{
+			dpWeight[i][j] = -1;
+		}
+	}
+}
+
+void ReadCase()
+{
+	cin >> noskipws;
+
+	int num;
+	char ch;
+
+	int x = 0;
+
+	while(cin >> num >> ch)
+	{
+		totalSum += num;
+		luggages[x++] = num;
+
+		if (' ' != ch)
+		{
+			break;
+		}
+	}
+
+	totalObject = x;
+
+	cin >> skipws;
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	int test;
+	cin >> test;
+
+	while(test--)
+	{
+		ReadCase();
+	}
 	return 0;
 }
