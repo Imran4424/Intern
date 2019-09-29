2c36c96dd36ebbc7df7b666f9d5eed942c90af96
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-29 19:34:47 +0600

    set 1

---
+++ Set 1/10616.DivisibleGroupSum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+typedef long long int lli;
+
+li number, query;
+
+void ReadCase()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+	cin >> number >> query;
+
+	while(0 != number && 0 != query)
+	{
+		ReadCase();
+
+		cin >> number >> query;
+	}
+
+	return 0;
+}
