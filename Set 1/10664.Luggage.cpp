071dc14135b0e0c4377a1a93ac68ac43a7707c4d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-09-29 07:50:08 +0600

    set 1

--- Set 1/10664.Luggage.cpp
+++ Set 1/10664.Luggage.cpp
@@ -7,30 +7,21 @@ const int wSize = 201;
 int luggages[mSize];
 int dpWeight[mSize][wSize];
 
-int totalSum, totalObject;
+int totalSum, totalObject, minDiff;
 
 void Init()
 {
 	for (int i = 0; i <= totalObject; ++i)
 	{
-		for (int i = 0; i <= totalSum; ++i)
+		for (int j = 0; j <= totalSum; ++j)
 		{
 			dpWeight[i][j] = -1;
 		}
 	}
 }
 
-int Absolute(int x)
-{
-	if (x < 0)
-	{
-		-x;
-	}
 
-	return x;
-}
-
-int Solve(int i, int currentSum)
+int FindMinDiff(int i, int currentSum)
 {
 	if (-1 != dpWeight[i][currentSum])
 	{
@@ -41,8 +32,24 @@ int Solve(int i, int currentSum)
 	{
 		int otherhalf = totalSum - currentSum;
 
+		if(currentSum == otherhalf)
+		{
+			return dpWeight[i][currentSum] = 0;
+		}
 
+		return dpWeight[i][currentSum] = 5;
 	}
+
+	int left = FindMinDiff(i+1, currentSum);
+
+	int right = FindMinDiff(i+1, currentSum + luggages[i]);
+
+	if (left < right)
+	{
+		return dpWeight[i][currentSum] = left;
+	}
+
+	return dpWeight[i][currentSum] = right;
 }
 
 void ReadCase()
@@ -67,17 +74,43 @@ void ReadCase()
 
 	totalObject = x;
 
+	for (int i = 0; i < totalObject; ++i)
+	{
+		cout << luggages[i] << " ";
+	}
+
+	cout << endl;
+
 	cin >> skipws;
+}
+
+void Display()
+{
+	if (0 == minDiff)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
 }
 
 int main(int argc, char const *argv[])
 {
+	freopen("input.txt","r", stdin);
+	freopen("output.txt", "w", stdout);
+
 	int test;
 	cin >> test;
 
 	while(test--)
 	{
 		ReadCase();
+
+		minDiff = FindMinDiff(0, 0);
+
+		Display();
 	}
 	return 0;
 }
