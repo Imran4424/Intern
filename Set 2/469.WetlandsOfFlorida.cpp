27b574c04ee97e71cd776d62376bad6aee73724d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-01 00:55:38 +0600

    set 2

--- Set 2/469.WetlandsOfFlorida.cpp
+++ Set 2/469.WetlandsOfFlorida.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
+
+const int lSize = 100;
+
+char land[100][100];
+
+void ReadCase()
+{
+	while()
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	int test;
+	scanf("%d", &test);
+
+	getchar();
+	getchar();
+
+	while(test--)
+	{
+		ReadCase();
+		Display();
+
+		if (test)
+		{
+			printf("\n");
+		}
+	}
+
 	return 0;
 }
