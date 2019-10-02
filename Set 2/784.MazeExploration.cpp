a9b402812319cb68ec6f74ff1b997fbf050a885e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-02 20:34:49 +0600

    set 2

--- Set 2/784.MazeExploration.cpp
+++ Set 2/784.MazeExploration.cpp
@@ -2,6 +2,36 @@
 #include <stdio.h>
 using namespace std;
 
+const int rSize = 35;
+const int cSize = 85;
+
+char grid[rSize][cSize];
+int row;
+
+
+void ReadCase()
+{
+	row = 0;
+
+	while(gets(grid[row]))
+	{
+		if('_' == grid[row][0])
+		{
+			break;
+		}
+
+		row++;
+	}
+}
+
+void Display()
+{
+	for (int i = 0; i <= row; ++i)
+	{
+		printf("%s\n", grid[i]);
+	}
+}
+
 int main(int argc, char const *argv[])
 {
 	int test;
@@ -9,7 +39,8 @@ int main(int argc, char const *argv[])
 
 	while(test--)
 	{
-		
+		ReadCase();
+		Display();
 	}
 
 
