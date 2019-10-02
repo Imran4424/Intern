0b0a463e38f79c7e15a450e3c2cc5141f6cf4c4f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-10-02 21:00:41 +0600

    set 2

--- Set 2/784.MazeExploration.cpp
+++ Set 2/784.MazeExploration.cpp
@@ -8,6 +8,46 @@ const int cSize = 85;
 char grid[rSize][cSize];
 int row;
 
+void FillTravel(int x, int y)
+{
+	grid[x][y] = '#';
+
+	if (' ' == grid[x - 1][y])
+	{
+		FillTravel(x - 1, y);
+	}
+
+	if (' ' == grid[x + 1][y])
+	{
+		FillTravel(x + 1, y);
+	}
+
+	if (' ' == grid[x][y - 1])
+	{
+		FillTravel(x, y - 1);
+	}
+
+	if (' ' == grid[x][y + 1])
+	{
+		FillTravel(x, y + 1);
+	}
+}
+
+void Solve()
+{
+	for (int i = 0; i <= row; ++i)
+	{
+		for(int j = 0; grid[i][j] != '0'; j++)
+		{
+			if ('*' == grid[i][j])
+			{
+				FillTravel(i, j);
+
+				return;
+			}
+		}
+	}
+}
 
 void ReadCase()
 {
@@ -34,6 +74,9 @@ void Display()
 
 int main(int argc, char const *argv[])
 {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
 	int test;
 	scanf("%d", &test);
 
